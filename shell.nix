073bc5258c371eaedbd54b86ee8909d29efadc3e
Manuel Bärenz <programming@manuelbaerenz.de>
2020-08-13 15:49:57 +0200

    Provide up to date versions of dependencies

--- shell.nix
+++ shell.nix
@@ -5,7 +5,20 @@
 let
   inherit (nixpkgs) pkgs;
 
-  haskellPackages = pkgs.haskell.packages.${compiler};
+  haskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super : {
+      essence-of-live-coding = haskellPackages.callHackageDirect {
+        pkg = "essence-of-live-coding";
+        ver = "0.2.0.1";
+        sha256 = "105dcfklvcjfgrdg47q36lln81mglds4jjq1g8drflmc59p2s0hk";
+      } {};
+      essence-of-live-coding-gloss = haskellPackages.callHackageDirect {
+        pkg = "essence-of-live-coding-gloss";
+        ver = "0.2.0.1";
+        sha256 = "11az32n65r7k1j1wm38zddilfzm1p6rrdd2w9rynrsslx8pddiqz";
+      } {};
+    };
+  };
 
   myPkgs = haskellPackages.extend (pkgs.haskell.lib.packageSourceOverrides {
     essence-of-live-coding-tutorial = ./.;
