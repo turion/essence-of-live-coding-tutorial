9ca5d725ae9278bc5cf46d7f678ce287c4a67a33
Manuel Bärenz <programming@manuelbaerenz.de>
2020-08-22 17:50:21 +0200

    WIP

--- shell.nix
+++ shell.nix
@@ -1,5 +1,6 @@
 { compiler ? "ghc883"
 , nixpkgs ? import <nixpkgs> {}
+, glfw ? true
 }:
 
 let
@@ -30,35 +31,77 @@ let
     ver = "0.7.1"; # For some reason, nixpkgs holds an older version of this
     sha256 = "07wli92lcvj4sh8xw1lx71b1bw1wimg6cr891h5027jszyvrcaz3";
   } {};
+  GLFW-b = (haskellPackages.callHackageDirect {
+    pkg = "GLFW-b";
+    ver = "1.4.8.4"; # Gloss needs an old version
+    sha256 = "05psnzz78gpv6mbm84zbgnadbz8rcis08ylakv89i0harz0vzf30";
+  } {}).overrideAttrs (_: { doCheck = false; }); # The test suite fails for whatever reason
 
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: {
       inherit
         essence-of-live-coding
-        essence-of-live-coding-gloss
         essence-of-live-coding-pulse
         essence-of-live-coding-warp
         http-client
+        # GLFW-b
       ;
+      GLFW-b = pkgs.haskell.lib.dontCheck super.GLFW-b;
+
+      gloss = super.gloss.overrideAttrs (old: {
+        configureFlags = if glfw
+          then ["-fglfw" "-f-glut" ]
+          else [];
+        buildInputs = old.buildInputs ++ (with self; [
+          GLFW-b
+        ]);
+      });
+
+      essence-of-live-coding-gloss = essence-of-live-coding-gloss.overrideAttrs (old: {
+        configureFlags = if glfw
+          then ["-fglfw" "-f-glut" ]
+          else [];
+        buildInputs = old.buildInputs ++ (with self; [
+          gloss
+          GLFW-b
+        ]);
+      });
+
+      essence-of-live-coding-tutorial = (super.callCabal2nix "essence-of-live-coding-tutorial" ./. {}).overrideAttrs (old: {
+        buildInputs = old.buildInputs ++ (with self; [
+          gloss
+          GLFW-b
+          essence-of-live-coding-gloss
+        ]);
+      });
+
     };
   };
 
   myPkgs = haskellPackages.extend (pkgs.haskell.lib.packageSourceOverrides {
-    essence-of-live-coding-tutorial = ./.;
+    # essence-of-live-coding-tutorial = ./.;
     # Uncomment the following lines if you have forked essence-of-live-coding and insert the appropriate path
     # essence-of-live-coding = ../essence-of-live-coding/essence-of-live-coding;
     # essence-of-live-coding-gloss = ../essence-of-live-coding/essence-of-live-coding-gloss;
+    GLFW-b = "1.4.8.4";
   });
 in
 myPkgs.shellFor {
   packages = p: with p; [
-    essence-of-live-coding-tutorial
+    # gloss
+    GLFW-b
+    GLURaw
+    OpenGLRaw
+    # essence-of-live-coding-tutorial
+    essence-of-live-coding
+    # essence-of-live-coding-gloss
+    essence-of-live-coding-pulse
+    essence-of-live-coding-warp
   ];
   buildInputs = with myPkgs; [
     cabal-install
     ghcid
     ghcide
     hlint
-    # Add further build tools as you like
   ];
 }
