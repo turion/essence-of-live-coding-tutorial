d6fef49f63facf48e3dc75236bb33a4de237c7a6
Manuel Bärenz <programming@manuelbaerenz.de>
2020-08-22 18:14:31 +0200

    WIP

--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@
 let
   inherit (nixpkgs) pkgs;
 
+  # Use self in overrides instead
   essence-of-live-coding = haskellPackages.callHackageDirect {
     pkg = "essence-of-live-coding";
     ver = "0.2.2";
@@ -31,11 +32,11 @@ let
     ver = "0.7.1"; # For some reason, nixpkgs holds an older version of this
     sha256 = "07wli92lcvj4sh8xw1lx71b1bw1wimg6cr891h5027jszyvrcaz3";
   } {};
-  GLFW-b = (haskellPackages.callHackageDirect {
-    pkg = "GLFW-b";
-    ver = "1.4.8.4"; # Gloss needs an old version
-    sha256 = "05psnzz78gpv6mbm84zbgnadbz8rcis08ylakv89i0harz0vzf30";
-  } {}).overrideAttrs (_: { doCheck = false; }); # The test suite fails for whatever reason
+  # GLFW-b = (haskellPackages.callHackageDirect {
+  #   pkg = "GLFW-b";
+  #   ver = "1.4.8.4"; # Gloss needs an old version
+  #   sha256 = "05psnzz78gpv6mbm84zbgnadbz8rcis08ylakv89i0harz0vzf30";
+  # } {}).overrideAttrs (_: { doCheck = false; }); # The test suite fails for whatever reason
 
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = self: super: {
@@ -46,16 +47,22 @@ let
         http-client
         # GLFW-b
       ;
-      GLFW-b = pkgs.haskell.lib.dontCheck super.GLFW-b;
+      GLFW-b = pkgs.haskell.lib.dontCheck (self.callHackageDirect {
+        pkg = "GLFW-b";
+        ver = "1.4.8.4"; # Gloss needs an old version
+        sha256 = "05psnzz78gpv6mbm84zbgnadbz8rcis08ylakv89i0harz0vzf30";
+      } {});
 
-      gloss = super.gloss.overrideAttrs (old: {
-        configureFlags = if glfw
-          then ["-fglfw" "-f-glut" ]
-          else [];
+      gloss = pkgs.haskell.lib.disableCabalFlag (pkgs.haskell.lib.enableCabalFlag (super.gloss.overrideAttrs (old: {
+        # configureFlags = if glfw
+        #   then ["-fglfw" "-f-glut" ]
+        #   else [];
         buildInputs = old.buildInputs ++ (with self; [
           GLFW-b
         ]);
-      });
+      })) "GLFW") "GLUT";
+
+      # gloss = pkgs.haskell.lib.disableCabalFlag (pkgs.haskell.lib.enableCabalFlag super.gloss "GLFW") "GLUT";
 
       essence-of-live-coding-gloss = essence-of-live-coding-gloss.overrideAttrs (old: {
         configureFlags = if glfw
@@ -83,20 +90,20 @@ let
     # Uncomment the following lines if you have forked essence-of-live-coding and insert the appropriate path
     # essence-of-live-coding = ../essence-of-live-coding/essence-of-live-coding;
     # essence-of-live-coding-gloss = ../essence-of-live-coding/essence-of-live-coding-gloss;
-    GLFW-b = "1.4.8.4";
+    # GLFW-b = "1.4.8.4";
   });
 in
 myPkgs.shellFor {
   packages = p: with p; [
     # gloss
-    GLFW-b
-    GLURaw
-    OpenGLRaw
-    # essence-of-live-coding-tutorial
-    essence-of-live-coding
+    # GLFW-b
+    # GLURaw
+    # OpenGLRaw
+    essence-of-live-coding-tutorial
+    # essence-of-live-coding
     # essence-of-live-coding-gloss
-    essence-of-live-coding-pulse
-    essence-of-live-coding-warp
+    # essence-of-live-coding-pulse
+    # essence-of-live-coding-warp
   ];
   buildInputs = with myPkgs; [
     cabal-install
